ce4cf79068b0f92af2c5eeaf2fb02af07e9d169a
Ashadullah Shawon <shawonashadullah@gmail.com>
2022-04-26 00:08:26 +0600

    Add files via upload

---
+++ aws-private-ec2-alb/ec2.tf
@@ -0,0 +1,22 @@
+#create a ec2 instance with existing vpc subnet
+resource "aws_instance" "dev-instance" {
+  ami           = "ami-002068ed284fb165b"
+  instance_type = "t2.micro"
+  subnet_id      = element(aws_subnet.private.*.id, 2)
+  vpc_security_group_ids = [ aws_security_group.lb.id ]
+  key_name = "dev-server"
+  user_data              = <<EOF
+  #!/bin/bash
+      sudo amazon-linux-extras enable nginx1 
+      sudo yum install -y nginx
+      sudo service nginx restart
+      sudo systemctl enable nginx.service
+      sudo service nginx status
+  EOF
+  tags = {
+    Name = "dev-ec2-server"
+  }
+
+}
+
+#terraform destroy -target RESOURCE_TYPE.NAME
---
+++ aws-private-ec2-alb/lb.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "default" {
+  name            = "ec2-lb"
+  subnets         = aws_subnet.public.*.id
+  security_groups = [aws_security_group.lb.id]
+}
+
+resource "aws_lb_target_group" "alb-tg" {
+  name        = "alb-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.ec2-vpc.id
+  target_type = "ip"
+  health_check {
+    interval            = 10
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ec2-ip" {
+  target_group_arn = aws_lb_target_group.alb-tg.arn
+  target_id        = aws_instance.dev-instance.private_ip
+  port             = 80
+}
+
+resource "aws_lb_listener" "alb-listener" {
+  load_balancer_arn = aws_lb.default.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.alb-tg.id
+    type             = "forward"
+  }
+}
+
